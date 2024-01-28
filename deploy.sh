eb3c3ca109e5c136a20d19f3d48de4d615a5ed59
Dedy Ibrahim <dedyibrahym23@gmail.com>
2024-01-28 22:13:12 +0700

    Prepairing CI/CD

--- deploy.sh
+++ deploy.sh
@@ -10,14 +10,9 @@ composer install
 php artisan migrate
 
 # Menyalin file konfigurasi
-cp .env.production .env
+cp .env. .env
 
 # Menjalankan perintah Laravel lainnya sesuai kebutuhan
-# Ambil nilai dari GitHub Secrets
-FTP_USERNAME=${{ secrets.FTP_USERNAME }}
-FTP_PASSWORD=${{ secrets.FTP_PASSWORD }}
-FTP_HOST= ${{ secrets.FTP_SERVER }}
-
 
 # Upload ke server FTP
-lftp -u "$FTP_USERNAME","$FTP_PASSWORD" -e "mirror -R ./ <REMOTE_PATH> ; quit" "$FTP_HOST"
+lftp -u furnixfu , Furnix@123 -e "mirror -R ./api.furnixfurniture.co.id ; quit" ftp.furnixfurniture.co.id
