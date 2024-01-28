c8bd29cba1e7e9da807aff4c5ab3e960777b166c
Dedy Ibrahim <dedyibrahym23@gmail.com>
2024-01-28 22:28:41 +0700

    Prepairing CI/CD

--- deploy.sh
+++ deploy.sh
@@ -13,7 +13,12 @@ composer install
 cp .env.production .env
 
 # Menjalankan perintah Laravel lainnya sesuai kebutuhan
+if ! command -v lftp &> /dev/null; then
+    echo "lftp not found. Installing lftp..."
+    sudo apt-get update
+    sudo apt-get install -y lftp
+fi
 
 # Upload ke server FTP
-lftp -u furnixfu,'Furnix@123' -e "mirror -R ./api.furnixfurniture.co.id ; quit" -p 21 ftp.furnixfurniture.co.id
+lftp -u furnixfu,'Furnix@123' -e "mirror -R /api.furnixfurniture.co.id/ ; quit" -p 21 ftp.furnixfurniture.co.id
 
